b2db690b52701631d5a6642031a7e9210f9ee0a7
Mahmoud Saad <m4saad@gmail.com>
2016-04-20 16:34:04 +0200

    Create func.js

---
+++ func.js
@@ -0,0 +1,10 @@
+ function email_good(email) {
+          var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+          return re.test(email);
+      }
+
+      function is_link(checkLink){
+         // var encodedURL = encodeURIComponent(checkLink);
+        var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+        return regexp.test(checkLink);
+      }
